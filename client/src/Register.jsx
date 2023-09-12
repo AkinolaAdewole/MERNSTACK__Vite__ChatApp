41d5191ea5f5f525d19ef76567b090799c8aa4b8
Akinola Adewole <adewoleakinola24@gmail.com>
2023-09-12 10:30:45 +0100

    first commit

---
+++ client/src/Register.jsx
@@ -0,0 +1,11 @@
+import React,{useState} from 'react'
+
+const Register = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+  return (
+    <div>Register</div>
+  )
+}
+
+export default Register
--- client/src/RegisterAndLoginForm.jsx
+++ client/src/RegisterAndLoginForm.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React,{useState, useContext} from 'react'
+import axios from "axios";
+import {UserContext} from "./UserContext.jsx";
 
 const RegisterAndLoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoginOrRegister, setIsLoginOrRegister] = useState('login');
+  const {setUsername:setLoggedInUsername, setId} = useContext(UserContext);
+  async function handleSubmit(ev) {
+    ev.preventDefault();
+    const url = isLoginOrRegister === 'register' ? 'register' : 'login';
+    const {data} = await axios.post(url, {username,password});
+    setLoggedInUsername(username);
+    setId(data.id);
+  }
+
   return (
     <div>RegisterAndLoginForm</div>
   )
